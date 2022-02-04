809de0e04993c08ec7c3ab16ca064d76ca3439f1
matinamehdizadeh <45507606+matinamehdizadeh@users.noreply.github.com>
2022-02-04 23:00:06 +0330

    Update LiveOV7670.ino

--- Code/LiveOV7670-master/src/LiveOV7670/LiveOV7670.ino
+++ Code/LiveOV7670-master/src/LiveOV7670/LiveOV7670.ino
@@ -1,3 +1,68 @@
+<<<<<<< HEAD
+
+
+
+
+#define trigPin 11
+#define echoPin 10
+#include <NewPing.h>
+
+// change setup.h to switch between buffered and pixel-by-pixel processing
+#include "setup.h"
+
+
+
+void setup() {
+  // This is not necessary and has no effect for ATMEGA based Arduinos.
+  // WAVGAT Nano has slower clock rate by default. We want to reset it to maximum speed
+  //CLKPR = 0x80; // enter clock rate change mode
+  //CLKPR = 0; // set prescaler to 0. WAVGAT MCU has it 3 by default.
+  Serial.begin (9600);
+  pinMode(6, OUTPUT);
+  
+
+
+  initializeScreenAndCamera();
+}
+NewPing sonar(trigPin, echoPin, 200);
+int counter = 0;
+float distance = 10;
+int end1 = 1;
+
+void loop() {
+  
+  
+  //Serial.println(counter);
+  //Serial.println(int(distance));
+  //delay(1000);
+  if (!(counter < (2*distance))){
+    delay(50);  // Wait 50ms between pings (about 20 pings/sec). 29ms    
+    unsigned int uS = sonar.ping(); // Send ping, get ping time in 
+    Serial.print("Ping: ");
+    distance = sonar.convert_cm(uS);
+    if (int(distance) != 0){
+      end1 = 1;
+      Serial.println("set new num");
+    }
+    Serial.print(distance); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+    Serial.println("cm");
+    counter = 0;
+  }
+  if (distance < 30){
+    Serial.println("turn on");
+    if (counter  < distance){
+      digitalWrite(6, HIGH);
+    }else{
+      Serial.print("turn off");
+        digitalWrite(6, LOW);
+    }
+  }
+  Serial.println(end1);
+  processFrame(int(distance), end1);
+  end1 = 0;
+  counter++;
+}
+=======
 
 
 
@@ -56,3 +121,4 @@ void loop() {
   
   counter++;
 }
+>>>>>>> 49a95cb9f550fe4027a04263b9cfc2870c533d4b
